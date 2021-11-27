9408e8d4a4de6b9a574b8bbd4112e5b59bc7030d
itzkumaran <kumaran4jobs@gmail.com>
2021-11-26 22:23:04 -0500

    module-5-working

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const PageNotFound = () => <h1> You got a wrong page dude..</h1>;
+
+export default PageNotFound;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import App from "./components/App";
+import "./index.css";
 
-myglobal = 4;
-
-function Hi() {
-  return <p>Hi.</p>;
-}
-
-render(<Hi />, document.getElementById("app"));
+render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("app")
+);
