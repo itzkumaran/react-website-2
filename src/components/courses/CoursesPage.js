818661029f1e67df7616fc9a186f5a607a04f4ae
itzkumaran <kumaran4jobs@gmail.com>
2021-12-04 21:22:24 -0500

    broken module-9 middle

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,9 +1,51 @@
 import React from "react";
+import { connect } from "react-redux";
+import * as courseActions from "../../redux/actions/courseActions";
+//import PropTypes from "prop-types";
 
 class CoursesPage extends React.Component {
+    state = {
+        course: {
+            title: ""
+        }
+    };
+
+    handleChange = (event) => {
+        const course = { ...this.state.course, title: event.target.value }
+        this.setState({ course });
+    }
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.props.dispatch(courseActions.createCourse(this.state.course));
+        alert('Courses');
+    }
     render() {
-        return <h2> Courses Page </h2>;
+        return (
+        <form onSubmit={this.handleSubmit}>
+            <h2> Courses </h2>
+            <h3> Add Course </h3>
+            <input 
+                type="text" 
+                onChange={this.handleChange}
+                value={this.state.course.title} 
+            />
+            <input 
+                type="submit" 
+                value="Save" 
+            />
+        </form>
+        );
     }
 }
 
-export default CoursesPage;
+// CoursesPage.propTypes = {
+//     dispatch: PropTypes.func.isRequired()
+// };
+
+function mapStateToProps(state) {
+    return {
+        courses: state.courses
+    };
+}
+
+export default connect(mapStateToProps)(CoursesPage);
